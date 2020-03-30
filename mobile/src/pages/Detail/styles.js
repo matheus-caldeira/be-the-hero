35308bc2ba833e4c24e29ea06b665e4d5110f553
Matheus Caldeira <matheuscardozo4@gmail.com>
2020-03-30 00:22:01 -0300

    Mobile da aplicação

---
+++ mobile/src/pages/Detail/styles.js
@@ -0,0 +1,80 @@
+import { StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight + 20,
+    },
+
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    incident: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#fff',
+        marginBottom: 6,
+        marginTop: 48,
+    },
+
+    incidentProperty: {
+        fontSize: 18,
+        color: '#41414d',
+        fontWeight: 'bold',
+        marginTop: 24,
+    },
+
+    incidentValue: {
+        marginTop: 8,
+        fontSize: 17,
+        color: '#737380'
+    },
+
+    contactBox: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#fff',
+        marginBottom: 6,
+        marginTop: 48,
+    },
+
+    heroTitle: {
+        fontWeight: 'bold',
+        fontSize: 22,
+        color: '#13131a',
+        lineHeight: 30,
+    },
+
+    heroDescription: {
+        fontSize: 18,
+        color: '#737380',
+        marginTop: 16,
+    },
+
+    actions: {
+        marginTop: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
+    action: {
+        backgroundColor: '#e82041',
+        borderRadius: 8,
+        height: 50,
+        width: '48%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    actionText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    
+})
